37927ec721a2611e09f13573a6f5df97164bfaf9
DoTanTu <95263083+DoTanTu@users.noreply.github.com>
2023-12-15 00:40:37 +0700

    thanh bui test author

--- server/sendo_server/router/AuthRouter.js
+++ server/sendo_server/router/AuthRouter.js
@@ -13,5 +13,8 @@ router.post('/login', [
 router.get('/protected', authenticateToken, (req, res) => {
     res.json({ message: 'This is a protected route', user: req.user });
 });
+router.get('/',  (req, res) => {
+    res.json({ message: 'This is a protected route' });
+});
 module.exports = router;
 
