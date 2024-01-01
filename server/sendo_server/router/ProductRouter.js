192e00da1bb5f847f197af28c610c13ad2368297
thanhbui230202 <thanhbui.230202@gmail.com>
2024-01-02 00:30:57 +0700

    update create product

--- server/sendo_server/router/ProductRouter.js
+++ server/sendo_server/router/ProductRouter.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const Authenticate = require("../middleware/AuthMiddleware");
 const productController = require('../controller/ProductController');
 router.get('/products', productController.getAllProduct);
-router.post('/create/product',productController.addNewProduct);
+router.post('/create/product',Authenticate.authenticateToken,productController.addNewProduct);
 module.exports = router;
