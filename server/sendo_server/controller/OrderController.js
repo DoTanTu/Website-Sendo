1c626b9b30cb7c9541df779fc23660ce17c6da9e
thanhbui230202 <thanhbui.230202@gmail.com>
2024-01-15 00:04:35 +0700

    update add order

--- server/sendo_server/controller/OrderController.js
+++ server/sendo_server/controller/OrderController.js
@@ -1,20 +1,32 @@
 const orderModel = require('../model/OrderModel');
 class OrderController{    
-  static async createOrder(req, res) {
-    try {
-        const {paymentId, cardId, totalAmount, addressDelivery, cartItems } = req.body;
-        const userId = req.user.id;  
-        const orderId = await orderModel.addOrder(userId, paymentId, cardId, totalAmount, addressDelivery, cartItems);
-        res.json({ success: true, orderId, message: 'Order placed successfully' });
-    } catch (error) {
-        console.error('Error placing order:', error);
-        res.status(500).json({ success: false, message: 'Error placing order' });
-    }
-}
+    static async createOrder(req, res) {
+        try {
+          const { paymentId, cartId, totalAmount, addressDelivery, cartItems, orderDate } = req.body;
+          const userId = req.user.id;
+    
+          const orderId = await orderModel.addOrder(userId, paymentId, cartId, totalAmount, orderDate, addressDelivery, cartItems);
+    
+          res.json({ success: true, orderId, message: 'Order placed successfully' });
+        } catch (error) {
+          console.error('Error placing order:', error);
+          res.status(500).json({ success: false, message: 'Error placing order' });
+        }
+      }
   static async getOrdersByUser(req, res) {
     const userId = req.user.id;        
     try {
-        const orders = await orderModel.getOrdersByUser(userId, req.connection);
+        const orders = await orderModel.getOrdersByUser(userId);
+        res.status(200).json({ orders });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }      
+  }
+  static async getOrdersDetailByUser(req, res) {
+    const userId = req.user.id;        
+    try {
+        const orders = await orderModel.getOrdersDetailByUser(userId);
         res.status(200).json({ orders });
     } catch (error) {
         console.error(error);
